9a47681a3037675196f89b662391912e7eba0daa
Samuel Fasanya <fasanyasam@yahoo.com>
2020-03-26 08:09:53 +0100

    firstScript.js

---
+++ firstScript.js
@@ -0,0 +1,13 @@
+// assign my  namme to a variable 
+var myName= "Samuel Fasanya";
+// assign my list of courses to an array 
+var myCourses=["HTML", "CSS", "JAVASCRIPT", "PYTHON"];
+// new empty array 
+var array=[];
+//iterate over and store results (even numbers) in the array 
+for (var i =0; i <= 200; i +=2 ){
+  array.push(i);
+  }
+console.log(myName)
+console.log(myCourses)
+console.log(array)
